8ecf39676efc44441684d1013bf6d118f7bff45e
Eurus Wagner <euruswagner@gmail.com>
2020-09-22 09:38:26 -0400

    initial commit

---
+++ VEW-Backend-API/src/main/java/com/revature/vew/VewApplication.java
@@ -0,0 +1,13 @@
+package com.revature.vew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VewApplication.class, args);
+    }
+
+}
